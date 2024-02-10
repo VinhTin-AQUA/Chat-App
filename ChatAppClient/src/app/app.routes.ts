59183e6bd7a45b1cd9ae19ca759f27079465abf5
VinhTin-AQUA <tinhovinh@gmail.com>
2024-02-11 00:13:00 +0700

    feat: add login page and register page

--- ChatAppClient/src/app/app.routes.ts
+++ ChatAppClient/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+  { path: '', component: RegisterComponent, title: 'Login' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'register', component: RegisterComponent, title: 'Register' },
+  { path: 'chat', component: ChatComponent, title: 'Chat' },
+];
---
+++ ChatAppClient/src/app/register/register.component.ts
@@ -0,0 +1,84 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+  avatarFileName: string = 'No avatar';
+  backgroundAvatar: string = "url('/assets/no-avatar.png')";
+  avatarAdd: any;
+  registerForm!: FormGroup;
+  isSubmit: boolean = false;
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      reEnterPassword: ['', [Validators.required]],
+    });
+
+    const imageUrl = '/assets/no-avatar.png';
+    // nếu người dùng không chọn avatar thì lấy avatar mặc định
+    this.http.get(imageUrl, { responseType: 'blob' }).subscribe((data) => {
+      const file = new File(['blob'], 'no-avatar.png', { type: 'image/png' });
+      this.avatarAdd = file;
+    });
+  }
+
+  onSelectedAvatar(event: any) {
+    const file = event.target.files[0];
+    this.avatarAdd = file;
+
+    console.log(this.avatarAdd);
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.backgroundAvatar = `url(${e.target.result})`;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onRegister() {
+    this.isSubmit = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    console.log(this.registerForm.value);
+  }
+
+  onChangeReenterPassword() {
+    if (
+      this.registerForm.controls['password'].value !==
+      this.registerForm.controls['reEnterPassword'].value
+    ) {
+      this.registerForm.controls['reEnterPassword'].markAsDirty({
+        onlySelf: true,
+      });
+      return;
+    }
+
+    this.registerForm.controls['reEnterPassword'].markAsPristine({
+      onlySelf: true,
+    });
+  }
+}
