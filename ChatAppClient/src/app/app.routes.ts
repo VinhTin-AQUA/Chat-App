5704bab586598e1bf7a72fbe80ab6b8e017f943e
VinhTin-AQUA <tinhovinh@gmail.com>
2024-02-12 00:57:58 +0700

    feat: add change password confirm

--- ChatAppClient/src/app/app.routes.ts
+++ ChatAppClient/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ConversationComponent } from './chat/conversation/conversation.component';
+import { ChangePasswordConfirmComponent } from './change-password-confirm/change-password-confirm.component';
 
 export const routes: Routes = [
 	{ path: '', redirectTo: 'chat', pathMatch: 'full' },
@@ -11,4 +12,5 @@ export const routes: Routes = [
 	{ path: 'register', component: RegisterComponent, title: 'Register' },
 	{ path: 'forgot-password', component: ForgotPasswordComponent, title: 'Forgot password' },
 	{ path: 'chat', loadChildren: () => import('./chat/chat.routes').then(r => r.routes) },
+	{ path: 'change-password-confirm', component: ChangePasswordConfirmComponent, title: 'Change password confirm' },
 ];
---
+++ ChatAppClient/src/app/change-password-confirm/change-password-confirm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-password-confirm',
+  standalone: true,
+  imports: [],
+  templateUrl: './change-password-confirm.component.html',
+  styleUrl: './change-password-confirm.component.scss'
+})
+export class ChangePasswordConfirmComponent {
+
+}
